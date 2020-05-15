9c627dc2cf2584f686ceb3f6cc8fb8f4aecbeb13
Davidsradd <57205283+Davidsradd@users.noreply.github.com>
2020-05-14 22:24:18 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-alert("Hello Rudy.")
-
-//simple animation/functions//
+//simple animation/function//
 function scrollAppear(){
     var introText = document.querySelector('.intro-text');
     var introPosition = introText.getBoundingClientRect().top;
@@ -24,99 +22,11 @@ $(document).ready(function(){
 
 //ajax//
 
-//hang man//
-var programming_languages = [
-    
-    "travel",
-    "jumpshot",
-    "layup",
-    "dribble",
-    "dunk",
-    "pass",
-    "shoot"
-	
-]
-
-let answer = '';
-let maxWrong = 6;
-let mistakes = 0;
-let guessed = [];
-let wordStatus = null;
-
-function randomWord() {
-  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+//function//
+function display(text)
+{
+    alert("Welcome " + text);
 }
 
-function generateButtons() {
-  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
-    `
-      <button
-        class="btn btn-lg btn-primary m-2"
-        id='` + letter + `'
-        onClick="handleGuess('` + letter + `')"
-      >
-        ` + letter + `
-      </button>
-    `).join('');
-
-  document.getElementById('keyboard').innerHTML = buttonsHTML;
-}
-
-function handleGuess(chosenLetter) {
-  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-  document.getElementById(chosenLetter).setAttribute('disabled', true);
-
-  if (answer.indexOf(chosenLetter) >= 0) {
-    guessedWord();
-    checkIfGameWon();
-  } else if (answer.indexOf(chosenLetter) === -1) {
-    mistakes++;
-    updateMistakes();
-    checkIfGameLost();
-    updateHangmanPicture();
-  }
-}
-
-
-
-function checkIfGameWon() {
-  if (wordStatus === answer) {
-    document.getElementById('keyboard').innerHTML = 'You Won!!!';
-  }
-}
-
-function checkIfGameLost() {
-  if (mistakes === maxWrong) {
-    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
-    document.getElementById('keyboard').innerHTML = 'You Lost!!!';
-  }
-}
-
-function guessedWord() {
-  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-
-  document.getElementById('wordSpotlight').innerHTML = wordStatus;
-}
-
-function updateMistakes() {
-  document.getElementById('mistakes').innerHTML = mistakes;
-}
-
-function reset() {
-  mistakes = 0;
-  guessed = [];
-  
-
-  randomWord();
-  guessedWord();
-  updateMistakes();
-  generateButtons();
-}
-
-document.getElementById('maxWrong').innerHTML = maxWrong;
-
-randomWord();
-generateButtons();
-guessedWord();
-
-//hangman//
+display("to my site, where I pay homage to one of the greatest to ever play the game.")
+//function//
